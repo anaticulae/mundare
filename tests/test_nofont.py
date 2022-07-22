5f8d7a0b96ad116ed814cd5f5b6331eed4b4b13d
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2022-07-22 13:02:42 +0200

    test(fontstore): do not dump any fontstore

---
+++ tests/test_nofont.py
@@ -0,0 +1,30 @@
+# =============================================================================
+# C O P Y R I G H T
+# -----------------------------------------------------------------------------
+# Copyright (c) 2022 by Helmut Konrad Fahrendholz. All rights reserved.
+# This file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+# =============================================================================
+
+import power
+import utila
+
+import tests
+
+
+def test_no_fontstore_bachelor56(td, mp):
+    """Run cleanup without any font information.
+
+    Ensure that no fontstore file is generated.
+    """
+    source = power.link(power.BACHELOR056_PDF)
+    utila.copy_content(
+        source,
+        td.tmpdir,
+        pattern='rawmaker__text_*.yaml',
+        unlock=True,
+    )
+    tests.run('-i . -o .', mp=mp)
+    # 5=> text, textpos and two backups, cleanup_translate and no fontstore
+    assert len(utila.file_list(td.tmpdir)) == 5
