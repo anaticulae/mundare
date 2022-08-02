fc67c2f249c1c456f3f7e7e44d6b5da4e8dbc591
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2022-08-02 19:51:12 +0200

    test(translate): adjust unit test

--- tests/test_translate.py
+++ tests/test_translate.py
@@ -23,7 +23,7 @@ def test_translate_lines(td):
     source, pages = power.BACHELOR037_PDF, '22,23,24'
     tests.utils.prepare(source, pages, td)
     # do not cache load_documents, do not use tests.run
-    utila.run('cleanup --cleanup --backup ' f'-i {td.tmpdir} -o {td.tmpdir}')
+    utila.run(f'cleanup --cleanup --backup -i {td.tmpdir} -o {td.tmpdir}')
     ptn = serializeraw.ptn_frompath(td.tmpdir)
     backup = serializeraw.ptn_frompath(td.tmpdir, backup=True)
     assert ptn != backup, 'cached load_documents? check backup=False'
